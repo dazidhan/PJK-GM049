022bd871efadb7ac09f5cd67d6852f5720e01cb3
Aldy Permana <aldypermana242@gmail.com>
2026-06-02 22:08:54 +0700

    Frontend

--- jabarulin-frontend/Backend/routes/apiRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getJabarulinRecommendation } = require('../controllers/recommendationController');
-
-// Endpoint untuk frontend (misal aplikasi mobile atau web)
-router.post('/recommend', getJabarulinRecommendation);
-
-module.exports = router;
